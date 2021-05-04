37b97d25ce515fe694e028861182e999b55c5fb4
Marion <marion.carambula@modop.com>
2021-05-03 19:48:57 -0500

    refactoring loading to use hooks

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,46 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const styles = {
-  content: {
-    fontSize: '35px',
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    marginTop: '20px',
-    textAlign: 'center',
-  }
-}
+    content: {
+        fontSize: '35px',
+        position: 'absolute',
+        left: '0',
+        right: '0',
+        marginTop: '20px',
+        textAlign: 'center',
+    },
+};
 
-export default class Loading extends React.Component {
-  state = { content: this.props.text }
-  componentDidMount () {
-    const { speed, text } = this.props
+export default function Loading({ text = 'Loading', speed = '300' }) {
+    const [content, setContent] = React.useState(text);
 
-    this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + '.' }))
-    }, speed)
-  }
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <p style={styles.content}>
-        {this.state.content}
-      </p>
-    )
-  }
+    React.useEffect(() => {
+        const interval = window.setInterval(() => {
+            setContent((content) => (content === text + '...' ? text : content + '.'));
+        }, speed);
+        return () => window.clearInterval(interval);
+    }, [speed, text]);
+
+    return <p style={styles.content}>{content}</p>;
 }
 
 Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired,
-}
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
-}
+    text: PropTypes.string,
+    speed: PropTypes.number,
+};
